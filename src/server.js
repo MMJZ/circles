1efcb607119601c1259a81de87fb6ce8606d8cad
Michael Agius <mike_agius@hotmail.co.uk>
2016-06-26 03:53:41 +0100

    init

--- src/server.js
+++ src/server.js
@@ -1,4 +1,103 @@
-// Helper Functions
+/*jslint bitwise: true, node: true */
+
+// Under the hood setup
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// Config static variables
+
+var tickLength = 20; // framerate = 1000/20 = 50fps.
+var playerRadius = 20;
+var playerMaxSpeed = 20;
+var playerAcceleration = 20;
+var outerBoundarySize = 6000;
+var innerBoundarySize = 3000;
+var gameLength = 1000 * 60;
+
+// Deduced static variables
+
+var maxTime = gameLength / tickLength;
+var boundarySpeed = innerBoundarySize / (2 * maxTime);
+var innerBoundaryStart = (outerBoundarySize - innerBoundarySize) / 2;
+
+// World variables
+
+var time = 0;
+var users = [];
+var sockets = {};
+var leaderboard = [];
+var leaderboardChanged = false;
+
+// World derived Variables
+
+function getInnerBoundaryPosition(){
+    return innerBoundaryStart + time * boundarySpeed;
+}
+
+function getOuterBoundaryPisition(){
+    return time * boundarySpeed;
+}
+
+// Game Ticking
+
+function doGameTick(){
+    time += 1;
+}
+
+// Socketing
+
+io.on('connection', function(socket) {
+    
+    console.log('info : user taking an interest');
+    
+    var currentPlayer;
+    
+    socket.on('nick', function(player) {
+       
+        console.log('info : ' + player.name + ' connecting');
+        
+        if(findIndex(users, player.id) > -1){
+            console.log('cerr : player is already connected');
+            socket.disconnect();
+        }else if(!validNick(player.name)){
+            socket.emit('kick', 'Nicked by the nick :(');
+            socket.disconnect();
+        }else{
+            console.log('info : ' + player.name + ' connected');
+            
+            currentPlayer = {
+                id: socket.id,
+                pos: getFreePosition(),
+                vel: {
+                    x: 0,
+                    y: 0
+                },
+                name: player.name,
+                radius: playerRadius,
+                lastUpdate: new Date().getTime()
+            };
+        }
+    });
+    
+    socket.on('disconnect', function () {
+        var indexx = findIndex(users, currentPlayer.id);
+        if (indexx > -1) users.splice(indexx, 1);
+        console.log('info : ' + currentPlayer.name + ' disconnected');
+        socket.broadcast.emit('playerDisconnect', { name: currentPlayer.name });
+    });
+});
+
+// Circle Functions
+
+function getFreePosition(){
+    return {
+        x: 0,
+        y: 0
+    };
+}
 
 function isTouching(a, b){
     var ar = a.radius, br = b.radius;
@@ -10,5 +109,34 @@ function isTouching(a, b){
 }
 
 function sq(x){
-    return x * x; 
+    return x * x;
+}
+
+function bashCircles(a, b){
+    var xDist = a.x - b.x, yDist = a.y - b.y,
+        xVel = b.vx - a.vx, yVel = b.vy - a.vy;
+    var dotProduct = xDist * xVel + yDist * yVel;
+    if(dotProduct > 0){
+        var collisionScale = dotProduct / (sq(xDist) + sq(yDist));
+        var xCollision = xDist * collisionScale,
+            yCollision = yDist * collisionScale;
+        var combinedMass = a.radius + b.radius;
+        var collWA = 2 * b.radius / combinedMass,
+            collWB = 2 * a.radius / combinedMass;
+        a.vel.set0(collWA * xCollision, collWA * yCollision);
+        b.vel.set0(-collWB * xCollision, -collWB * yCollision);
+    }
+}
+
+// Util Functions
+
+function findIndex(arr, id){
+    var len = arr.length;
+    while (len--) if (arr[len].id === id) return len;
+    return -1;
+}
+
+function validNick(nickname) {
+    var regex = /^\w*$/;
+    return regex.exec(nickname) !== null;
 }
